232a29802804570c8a636fbcf9a7364fb1a109ee
Bojidar Dermendjiev <116583072+BojidarDermendjiev@users.noreply.github.com>
2022-10-31 16:37:55 +0200

    Add files via upload

---
+++ Inheritance - Exercise/Animals/Person/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get => name; set => name = value; }
+        public virtual int Age { get => age; set { if (value >= 0)age = value; } }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public override string ToString()
+        => $"Name: {this.Name}, Age: {this.Age}";
+    }
+}
