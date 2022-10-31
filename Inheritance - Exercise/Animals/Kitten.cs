2190e9f667082821d3d1a1cb70da3033b84d1ef4
Bojidar Dermendjiev <116583072+BojidarDermendjiev@users.noreply.github.com>
2022-10-31 16:31:33 +0200

    Add files via upload

---
+++ Inheritance - Exercise/Animals/Kitten.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Kitten : Cat
+    {
+        private const string KITTEN_GENDER = "Female";
+
+        public override string Gender { get { return KITTEN_GENDER; } }
+
+        public Kitten(string name, int age)
+            : base(name, age, KITTEN_GENDER)
+        { }
+
+        public override string ProduceSound()
+            => "Meow";
+    }
+}
---
+++ Inheritance - Exercise/Animals/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input;
+            StringBuilder sb = new StringBuilder();
+            while ((input = Console.ReadLine()) != "Beast!")
+            {
+                string type = input;
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string gender = string.Empty;
+                if (tokens.Length > 2)
+                {
+                    gender = tokens[2];
+                }
+                switch (type)
+                {
+                    case "Dog": Dog dog = new Dog(name, age, gender);sb.AppendLine(dog.ToString()); break;
+                    case "Cat": Cat cat = new Cat(name, age, gender); sb.AppendLine(cat.ToString()); break;
+                    case "Frog": Frog frog = new Frog(name, age, gender); sb.AppendLine(frog.ToString()); break;
+                    case "Kitten": Kitten kitten = new Kitten(name, age); sb.AppendLine(kitten.ToString()); break;
+                    case "Tomcat": Tomcat tomcat = new Tomcat(name, age); sb.AppendLine(tomcat.ToString()); break;
+                    default: throw new ArgumentException("Invalid Input!");
+                }
+     
+            }
+            Console.WriteLine(sb.ToString());            
+        }
+    }
+}
