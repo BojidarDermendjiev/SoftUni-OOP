14b13531480f21372a0a6599eaa82ac03f6af45e
Bojidar Dermendjiev <116583072+BojidarDermendjiev@users.noreply.github.com>
2022-10-31 16:32:27 +0200

    Add files via upload

---
+++ Inheritance - Exercise/Animals/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption { get => this.DefaultFuelConsumption; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+            DefaultFuelConsumption = 1.25;
+        }
+        public virtual void Drive(double kilometers) 
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
