bb1a5b26a50a534190ea668d6fd8502bebebf0d6
Bojidar Dermendjiev <116583072+BojidarDermendjiev@users.noreply.github.com>
2022-10-31 16:36:11 +0200

    Add files via upload

---
+++ Inheritance - Exercise/Animals/Zoo/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        protected string name;
+        public string Name { get => name; set => name = value; }
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Inheritance - Exercise/Animals/Zoo/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Action<List<Animal>> print = animals => Console.WriteLine(string.Join(Environment.NewLine, animals)); 
+            List<Animal> animals = new List<Animal>();
+            Animal animal = new Animal("animal");
+            animals.Add(animal);
+            Mammal mammal = new Mammal("Mammal");
+            animals.Add((Animal)mammal);
+            Reptile reptile = new Reptile("Reptile");
+            animals.Add((Animal)reptile);
+            Lizard lizard = new Lizard("Lizard");
+            animals.Add((Animal)lizard);
+            Snake snake = new Snake("Snake");
+            animals.Add((Animal)snake);
+            Gorilla gorilla = new Gorilla("Gorilla");
+            animals.Add((Animal)gorilla);
+            Bear bear = new Bear("Bear");
+            animals.Add((Animal)bear);
+            print(animals);
+        }
+    }
+}
